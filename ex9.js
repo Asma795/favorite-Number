634965c52196f10c4441171b7f6f7e0444e07aa6
Asma Shahid <asmashahid795@gmail.com>
2024-03-03 00:41:39 +0500

    Add files via upload

---
+++ ex9.js
@@ -0,0 +1,2 @@
+var favorite_Number = 79;
+console.log("My favorite number: ".concat(favorite_Number));
---
+++ ex9.ts
@@ -0,0 +1,2 @@
+let favorite_Number: number = 79;
+console.log(`My favorite number: ${favorite_Number}`);
